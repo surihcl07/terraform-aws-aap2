77eb3f4bf65b45dfe5f5ede753ea5b00f9f76cba
surendra <surihcl07@gmail.com>
2022-09-16 10:22:41 +0530

    fixed depends on for RDS

--- rds-security-group.tf
+++ rds-security-group.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "rds" {
   name   = "aap2_rds"
   vpc_id = module.vpc.vpc_id
   depends_on = [
-    aws_security_group.bastion_sg, aws_security_group.controller_sg.id
+    aws_security_group.bastion_sg, aws_security_group.controller_sg
 
   ]
 
@@ -10,15 +10,14 @@ resource "aws_security_group" "rds" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    security_groups = [ aws_security_group.bastion_sg, aws_security_group.controller_sg.id ]
+    security_groups = [ aws_security_group.bastion_sg.id, aws_security_group.controller_sg.id ]
   }
 
   egress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    security_groups = [ aws_security_group.bastion_sg, aws_security_group.controller_sg.id ]
+    security_groups = [ aws_security_group.bastion_sg.id, aws_security_group.controller_sg.id ]
   }
 
   tags = {
